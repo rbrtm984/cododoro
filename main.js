e2d0d9841dc244c753e58589e6cc186808be7e8d
Jeff Levin <levinjn@gmail.com>
2023-08-30 21:15:22 -0400

    fixed button functionality

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
 
-function playAlarm () {
-    const audio = new Audio("sounds/thats-amore.mp3");
-    // audio.setAttribute("src", "sounds/thats-amore.mp3");
-    audio.play();
-}
+// function playAlarm () {
+//     const audio = new Audio("sounds/thats-amore.mp3");
+//     // audio.setAttribute("src", "sounds/thats-amore.mp3");
+//     audio.play();
+// }
 
-const button = document.getElementById("theButton");
-button.onclick = playAlarm();
+// const button = document.getElementById("theButton");
+// button.onclick = playAlarm();
 
 // setTimeout should invoke the playAlarm function after 25 minutes
+
+let myAudio = document.getElementById('audio');
+let playButton = document.getElementById('theButton');
+
+function player() { 
+    playButton.addEventListener("click", function(){ myAudio.play()})
+    };  
+
+document.addEventListener("DOMContentLoaded", player);
